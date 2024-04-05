a37580d011036f27c7521f68cc80314e9736167c
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-04-05 12:59:10 +0530

    installed cors and cookie parcer

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv"
 import ConnectDB from './db/index.js';
+import { app } from './app.js';
 dotenv.config(
     {
         path: './env'
     }
 )
-ConnectDB();
+ConnectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log('listening on port '+process.env.PORT)
+    })
+})
+.catch((err)=>console.error("MONGO DB error: " + err))
