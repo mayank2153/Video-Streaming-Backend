1da0f5f4ae4003d63a6fd71e9d8be25e81aed407
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-04-26 11:47:54 +0530

    solved minor bug in changeCurrentPassword

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -215,7 +215,7 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
 
 const changeCurrentPassword= asyncHandler(async(req,res)=>{
     const {oldPassword,newPassword} = req.body;
-    const user= await User.findById(req.user.id);
+    const user= await User.findById(req.user?._id);
     const isPasswordCorrect=await user.isPasswordCorrect(oldPassword);
     if(!isPasswordCorrect){
         throw new ApiError("400","Invalid Old Password");
@@ -228,6 +228,11 @@ const changeCurrentPassword= asyncHandler(async(req,res)=>{
 
     return res.status(200)
     .json( new ApiResponse(200,{},"Password Changed Successfully"));
+})
+
+const getCurrentUser=asyncHandler(async(req,res)=>{
+    return res.status(200)
+    .json(new ApiResponse(200,req.user,"User fetched Successfully"));
 })
 
 export {registerUser,loginUser,logOutUser,refreshAccessToken, changeCurrentPassword}
