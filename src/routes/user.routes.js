e303ad58aa3b27766121fcb8e433fe2016a9abed
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-04-30 12:22:42 +0530

    "Update user routes to include new controller functions"
    This commit updates the user routes file to include new controller functions such as changing the current password, getting the current user, updating account details, updating user avatar, updating user cover image,

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logOutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logOutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -22,5 +22,11 @@ router.route("/login").post(loginUser);
 // Secured Route
 router.route("/logout").post(verifyJWT,logOutUser);
 router.route("/refresh-token").post(refreshAccessToken)
-
+router.route("/change-password").post(verifyJWT,changeCurrentPassword);
+router.route("/current-user").get(verifyJWT,getCurrentUser);
+router.route("/update-account").patch(verifyJWT,updateAccountDetails);
+router.route("/avatar").patch(verifyJWT,updateUserAvatar);
+router.route("/cover-image").patch(verifyJWT,updateUserCoverImage);4
+router.route("/:c/userName").get(verifyJWT,getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT,getWatchHistory);
 export default router
